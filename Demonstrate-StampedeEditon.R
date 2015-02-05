b14fc04eb635b57fa7ec2bb4a35338ee46a709c4
Talleyman <Talleyman@users.noreply.github.com>
2015-02-05 13:53:58 -0500

    Create Demonstrate-StampedeEditon.R

---
+++ Demonstrate-StampedeEditon.R
@@ -0,0 +1,35 @@
+#Demonstrate-Stampede Edition (Feb. 2, 2015)
+#Original program by: Dustin Landers
+#Stampede Edition by: Stephen Talley and Marco Martinez
+
+require(getopt)
+require(sciplot)
+
+readFiles <- function(dir) {
+  setwd(dir)
+  files <- (Sys.glob("*.txt"))
+  listOfFiles <- lapply(files, function(x) read.table(x, header=TRUE))
+  return(listOfFiles)
+}
+
+CleanData<-function(data){
+  names<-list("AUC","MAE","H","H2")
+  for (file in data){
+    for (i in names(file)){
+      if ([[i]] not in names){
+        file[[i]]<-NULL
+      }
+    }
+  }
+  return(data)
+}
+
+MakeAUCPlot<-function(data, AUC.plot.title="My AUC Plot"){
+  pdf(file=AUC.plot.title)
+  lineplot.CI(data$Herit, data$AUC, data$Pop.Structure, type="b", 
+              main=AUC.plot.title)
+  dev.off()
+}
+MakeMAEPlot<-function(data, MAE.plot.title="My MAE Plot"){
+  pdf(file)
+}
