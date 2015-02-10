e94189f4f99313f394306b3d8bcbf04a66f6e6c4
Talleyman <stephen522@earthlink.net>
2015-02-10 12:51:08 -0500

    Changed arguments for plot functions

--- Demonstrate-StampedeEditon.R
+++ Demonstrate-StampedeEditon.R
@@ -12,21 +12,21 @@ readFiles <- function(dir) {
   return(listOfFiles)
 }
 
-CleanData<-function(data, col1, col2,col3=NULL,col4=NULL){
-  names<-c(col1,col2,col3,col4)
+CleanData<-function(data, col1, col2,col3=NULL,col4=NULL,col5=NULL){
+  names<-c(col1,col2,col3,col4,col5)
   newData<-data[names]
   return(newData)
 }
 
-MakeAUCPlot<-function(data, AUC.plot.title="My AUC Plot"){
+MakeAUCPlot<-function(Her, Pop=NULL, data, AUC.plot.title="My AUC Plot"){
   pdf(file=AUC.plot.title)
-  lineplot.CI(Herit, data$AUC, Pop.Structure, type="b", 
+  lineplot.CI(Herit, data$AUC, Pop, type="b", 
               main=AUC.plot.title, xlab="Heritability Coefficient", ylab="Mean AUC")
   dev.off()
 }
-MakeMAEPlot<-function(data, MAE.plot.title="My MAE Plot"){
+MakeMAEPlot<-function(Her, Pop=NULL, data, MAE.plot.title="My MAE Plot"){
   pdf(file=MAE.plot.title)
-  lineplot.CI(Herit, data$MAE, Pop.Structure, type="b", 
+  lineplot.CI(Her, data$MAE, Pop, type="b", 
   main=MAE.plot.title, xlab="Heritability Coefficient", ylab="Mean MAE")
   dev.off()
 }
@@ -43,8 +43,22 @@ options <- matrix(c("dir","dir",1,"character"
 all.opts<-getopt(options,args)
 
 mydata<-readFiles(dir)
+#Add in columns for heritability and population structure to dataset
+mydata$Herit<-NA
+for (i in 1:length(herit.strings)) {
+  newData$Herit <- ifelse(sapply(mydata$Name,function(x) grepl(herit.strings[[i]],x)),
+                          herit.values[[i]],newData$Herit)
+}
+mydata$Pop.Struct<-NA
+for (i in 1:length(struct.strings)) {
+  newData$Structure <- ifelse(sapply(mydata$Name,function(x) grepl(struct.strings[[i]],x)),
+                              struct.values[[i]],newData$Structure)
+}
 Filtr.list<-list()
 #For loop to append filtered data to new list
 for (i in 1:length(dir())){
-  Filtr.list[[length(Filtr.list)+1]]<-CleanData(mydata[[i]],"AUC","MAE")
+  Filtr.list[[length(Filtr.list)+1]]<-CleanData(data[[i]],"AUC","MAE","Herit","Pop.Structure")
 }
+mydata<-NULL #Clean out the original data set to save some memory
+MakeAUCPlot(Herit.List, Pop.Struct, Filtr.list, AUC.plot.title)
+MakeMAEPlot(Herit.List, Pop.Struct, Filtr.list, MAE.plot.title)
