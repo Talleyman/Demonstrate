f971f2c9c0fabc36c894a57ce6e2472f9aa7305f
Talleyman <stephen522@earthlink.net>
2015-02-27 17:00:14 -0500

    Moved all under one function

--- Demonstrate-StampedeEditon.R
+++ Demonstrate-StampedeEditon.R
@@ -5,17 +5,13 @@
 require(getopt)
 require(sciplot)
 
+<<<<<<< HEAD
+=======
 readFiles <- function(dir) {
   setwd(dir)
   files <- (Sys.glob("*.txt"))
   listOfFiles <- lapply(files, function(x) read.table(x, header=TRUE))
   return(listOfFiles)
-}
-
-CleanData<-function(data, col1, col2,col3=NULL,col4=NULL,col5=NULL){
-  names<-c(col1,col2,col3,col4,col5)
-  newData<-data[names]
-  return(newData)
 }
 
 MakeAUCPlot<-function(Her, Pop=NULL, data, AUC.plot.title){
@@ -32,6 +28,7 @@ MakeMAEPlot<-function(Her, Pop=NULL, data, MAE.plot.title){
 }
 #Main function
 #First, get command line arguments
+>>>>>>> origin/master
 args<-commandArgs(TRUE)
 options <- matrix(c("dir","d",1,"character"
                     "AUC-plot-title","a",2,"character",
@@ -42,28 +39,50 @@ options <- matrix(c("dir","d",1,"character"
                     "struct-values1","o",0,"character"),
                   ncol=4,byrow=TRUE)
 all.opts<-getopt(options,args)
-if ( is.null(all.opts$AUC.plot.title ) ){ all.opts$AUC.plot.title = "My AUC Plot" }
-if ( is.null(all.opts$MAE.plot.title ) ){ all.opts$MAE.plot.title = "My MAE plot" }
-
 for (i in 1:length(all.opts)){
   assign(possibles[[i]],all.opts[i])
 }
-mydata<-readFiles(dir)
-#Add in columns for heritability and population structure to dataset
-for (i in length(mydata)){
-  mydata[[i]]$Herit<-NA
-  for (j in 1:length(herit.strings)) {
-    myData$Herit <- ifelse(sapply(mydata[[i]]$Names,function(x) grepl(herit.strings[[j]],x)),
-                           herit.values[[j]],mydata[[i]]$Herit)
+Demonstrate <- function(dir, AUC.plot.title="Mean AUC By Population Structure and Heritability", MAE.plot.title="Mean MAE By Population Structure and Heritability",
+                        herit.strings=list("_03_","_04_","_06_") ,herit.values=list(0.3,0.4,0.6),struct.strings=NULL,struct.values=NULL {
+  
+  readFiles <- function(dir) {
+    setwd(dir)
+    files <- (Sys.glob("*.txt"))
+    listOfFiles <- lapply(files, function(x) read.table(x, header=TRUE))
+    return(listOfFiles)
   }
-}
-for (i in length(mydata)){
-  mydata[[i]]$Pop.Struct<-NA
-  for (j in 1:length(struct.strings)) {
-    myData[[i]]$Structure <- ifelse(sapply(mydata[[i]]$Names,function(x) grepl(struct.strings[[j]],x)),
-                               struct.values[[j]],mydata[[i]]$Structure)
+  CreateLabels<-function(data){
+    mapply(cbind, data, "Herit"=NA, SIMPLIFY=FALSE)
+    for (j in 1:length(herit.strings)) {
+      data$Herit <- ifelse(sapply(data$Names,function(x) grepl(herit.strings[[j]],x)),
+                             herit.values[[j]],data$Herit)
+    }
+    if (struct.strings !=NULL){
+      mapply(cbind, mydata, "Struct"=NA, SIMPLIFY=FALSE)
+      for (j in 1:length(struct.strings)) {
+        data$Structure <- ifelse(sapply(data$Names,function(x) grepl(struct.strings[[j]],x)),
+                                 struct.values[[j]],data$Structure)
+      }
+    }
   }
+  MakeAUCPlot<-function(Herit, Struct=NULL, data, AUC.plot.title){
+    pdf(file=AUC.plot.title)
+    lineplot.CI(data$Herit, data$AUC, data$Struct, type="b", main=AUC.plot.title, 
+                xlab="Heritability Coefficient", ylab="Mean AUC")
+    dev.off()
+  }
+  MakeMAEPlot<-function(Her, Struct=NULL, data, MAE.plot.title){
+    pdf(file=MAE.plot.title)
+    lineplot.CI(data$Herit, data$MAE, data$Struct, type="b", 
+                main=MAE.plot.title, xlab="Heritability Coefficient", ylab="Mean MAE")
+    dev.off()
+  }
+  myData<-readFiles(dir)
+  newData<-CreateLabels(myData)
+  return(newData)
 }
+<<<<<<< HEAD
+=======
 Filtr.list<-list()
 #For loop to append filtered data to new list
 for (i in 1:length(dir())){
@@ -72,3 +91,4 @@ for (i in 1:length(dir())){
 mydata<-NULL #Remove original data to save space
 MakeAUCPlot(Herit.List, Pop.Struct, Filtr.list, AUC.plot.title)
 MakeMAEPlot(Herit.List, Pop.Struct, Filtr.list, MAE.plot.title)
+>>>>>>> origin/master
