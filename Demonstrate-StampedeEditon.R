eefb454a509acaf2c6ee6da4f4aa90666908c01f
Talleyman <stephen522@earthlink.net>
2015-02-27 17:03:12 -0500

    Fixed Github mess up...

--- Demonstrate-StampedeEditon.R
+++ Demonstrate-StampedeEditon.R
@@ -5,30 +5,8 @@
 require(getopt)
 require(sciplot)
 
-<<<<<<< HEAD
-=======
-readFiles <- function(dir) {
-  setwd(dir)
-  files <- (Sys.glob("*.txt"))
-  listOfFiles <- lapply(files, function(x) read.table(x, header=TRUE))
-  return(listOfFiles)
-}
-
-MakeAUCPlot<-function(Her, Pop=NULL, data, AUC.plot.title){
-  pdf(file=AUC.plot.title)
-  lineplot.CI(Herit, data$AUC, Pop, type="b", 
-              main=AUC.plot.title, xlab="Heritability Coefficient", ylab="Mean AUC")
-  dev.off()
-}
-MakeMAEPlot<-function(Her, Pop=NULL, data, MAE.plot.title){
-  pdf(file=MAE.plot.title)
-  lineplot.CI(Her, data$MAE, Pop, type="b", 
-  main=MAE.plot.title, xlab="Heritability Coefficient", ylab="Mean MAE")
-  dev.off()
-}
 #Main function
 #First, get command line arguments
->>>>>>> origin/master
 args<-commandArgs(TRUE)
 options <- matrix(c("dir","d",1,"character"
                     "AUC-plot-title","a",2,"character",
@@ -43,7 +21,7 @@ for (i in 1:length(all.opts)){
   assign(possibles[[i]],all.opts[i])
 }
 Demonstrate <- function(dir, AUC.plot.title="Mean AUC By Population Structure and Heritability", MAE.plot.title="Mean MAE By Population Structure and Heritability",
-                        herit.strings=list("_03_","_04_","_06_") ,herit.values=list(0.3,0.4,0.6),struct.strings=NULL,struct.values=NULL {
+                        herit.strings=list("_03_","_04_","_06_") ,herit.values=list(0.3,0.4,0.6),struct.strings=NULL,struct.values=NULL) {
   
   readFiles <- function(dir) {
     setwd(dir)
@@ -81,17 +59,3 @@ Demonstrate <- function(dir, AUC.plot.title="Mean AUC By Population Structure an
   newData<-CreateLabels(myData)
   return(newData)
 }
-<<<<<<< HEAD
-=======
-Filtr.list<-list()
-#For loop to append filtered data to new list
-for (i in 1:length(dir())){
-  Filtr.list[[length(Filtr.list)+1]]<-CleanData(mydata[[i]],"AUC","MAE","Herit","Pop.Structure")
-}
-mydata<-NULL #Remove original data to save space
-MakeAUCPlot(Herit.List, Pop.Struct, Filtr.list, AUC.plot.title)
-MakeMAEPlot(Herit.List, Pop.Struct, Filtr.list, MAE.plot.title)
-<<<<<<< HEAD
->>>>>>> origin/master
-=======
->>>>>>> origin/master
