a0cd298c2cea12678fa4ee910929a55d30c7d9a4
Talleyman <stephen522@earthlink.net>
2015-03-05 12:33:48 -0500

    Added wrapper and removed alternate version

--- Demonstrate-StampedeEditon.R
+++ Demonstrate-StampedeEditon.R
@@ -18,10 +18,10 @@ options <- matrix(c("dir","d",1,"character",
                     "herit.value1","V",1,"double",
                     "herit.value2","l",1,"double",
                     "herit.value3","u",1,"double",
-                    "struct.string1","S",0,"character",
-                    "struct.string2","s",0,"character",
-                    "struct.value1","O",0,"logical",
-                    "struct.value2","o",0,"logical"),
+                    "struct.string1","S",1,"character",
+                    "struct.string2","s",1,"character",
+                    "struct.value1","O",1,"logical",
+                    "struct.value2","o",1,"logical"),
                   ncol=4,byrow=TRUE)
 all.opts<-getopt(options,args)
 Demonstrate <- function(dir, AUC.plot.title="Mean AUC By Population Structure and Heritability", MAE.plot.title="Mean MAE By Population Structure and Heritability",
@@ -39,8 +39,8 @@ Demonstrate <- function(dir, AUC.plot.title="Mean AUC By Population Structure an
       data$Herit <- ifelse(sapply(data$Names,function(x) grepl(herit.strings[[j]],x)),
                              herit.values[[j]],data$Herit)
     }
-    if (struct.strings !=NULL){
-      mapply(cbind, mydata, "Struct"=NA, SIMPLIFY=FALSE)
+    if (length(struct.strings) > 1){
+      mapply(cbind, data, "Struct"=NA, SIMPLIFY=FALSE)
       for (j in 1:length(struct.strings)) {
         data$Structure <- ifelse(sapply(data$Names,function(x) grepl(struct.strings[[j]],x)),
                                  struct.values[[j]],data$Structure)
@@ -63,8 +63,10 @@ Demonstrate <- function(dir, AUC.plot.title="Mean AUC By Population Structure an
   newData<-CreateLabels(myData)
   return(newData)
 }
-Demonstrate(dir=all.opts$dir, all.opts$AUC-plot-title, all.opts$MAE-plot-title, 
+
+dat<-Demonstrate(dir=all.opts$dir, all.opts$AUC-plot-title, all.opts$MAE-plot-title, 
             herit.strings=list(all.opts$herit.string1,all.opts$herit.string2,all.opts$herit.string3), 
             herit.values=list(all.opts$herit.value1,all.opts$herit.value2,all.opts$herit.value3),
             struct.strings=list(all.opts$struct.string1,all.opts$struct.string2),
             struct.values=list(all.opts$struct.value1,all.opts$struct.value2))
+print(dat)
